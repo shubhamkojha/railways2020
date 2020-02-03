8e145967e5a1d6041ad0ff583b1eadb932e6a71f
Shubham Kumar Ojha <shubhamkojha@Shubhams-MacBook-Pro.local>
2020-02-03 13:04:30 +0530

    procfile added

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const app = express();
 const trainStatus = require("./api/trainStatus");
 const mongoose = require("mongoose");
 
-// mongoose.connect(
-//     "mongodb://shubham4863:shubhamojha@cluster0-shard-00-00-n94o9.mongodb.net:27017,cluster0-shard-00-01-n94o9.mongodb.net:27017,cluster0-shard-00-02-n94o9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-//     {
-//       useMongoClient: true
-//     }
-//   );
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
