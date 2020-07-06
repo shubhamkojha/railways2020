7a9041426d8c82f3ad582b7deba2799760b1fdf7
shubhamiitbhu <shubhamk.ojha.civ16@iitbhu.ac.in>
2020-07-06 20:41:17 +0530

    break trains by 1

--- functions/algorithms/stationCodeValidator.js
+++ functions/algorithms/stationCodeValidator.js
@@ -1,14 +1,16 @@
 const stationCodetoName = require('../data/stationCodetoName.json');
 
-module.exports = function(){
-
-    this.stationCodeValidator = function(origin, destination)
-    {
-        if(stationCodetoName[origin.toUpperCase()] !== undefined && stationCodetoName[destination.toUpperCase()] !== undefined)
-        {
-            return true;
-        }
-        
-        return false;
-    }
-}
+module.exports = function() {
+	this.stationCodeValidator = function(origin, destination) {
+		try {
+			if (
+				stationCodetoName[origin.toUpperCase()] !== undefined &&
+				stationCodetoName[destination.toUpperCase()] !== undefined
+			) {
+				return true;
+			}
+		} catch (error) {
+			return false;
+		}
+	};
+};
