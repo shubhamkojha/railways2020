5e25bca8dae7732e1c07a69196b1d69c1a1942e3
shubhamiitbhu <shubhamk.ojha.civ16@iitbhu.ac.in>
2020-07-27 23:21:50 +0530

    proxy server created

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+require('dotenv').config();
 var cors = require('cors');
 
 const directTrains = require('./api/directTrains.js');
 const singleBreakTrains = require('./api/singleBreakTrains.js');
 const stationNameToCode = require('./api/stationNameToCode.js');
 const trainName = require('./api/trainName.js');
+const bookingProxy = require('./api/bookingProxy.js');
+const enquiryProxy = require('./api/enquiryProxy.js');
 
 app.use(cors());
 app.use(express.json());
@@ -16,5 +20,7 @@ app.use('/direct-trains/', directTrains);
 app.use('/alternate-trains', singleBreakTrains);
 app.use('/station-name-to-code', stationNameToCode);
 app.use('/train-name', trainName);
+app.use('/booking-proxy', bookingProxy);
+app.use('/enquiry-proxy', enquiryProxy);
 
 module.exports = app;
